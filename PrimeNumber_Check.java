29eb726260115bff0c682e4f581129bf809d3a9f
Sri Vallinayagam V.S. <151116310+srivallinayagam@users.noreply.github.com>
2025-05-07 14:13:44 +0530

    PrimeNumber_Check.java

--- PrimeNumber_Check.java
+++ PrimeNumber_Check.java
@@ -4,7 +4,7 @@ public class PrimeNumber_Check {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to check Prime:");
-        int number = sc.nextInt(); // Example number
+        int number = sc.nextInt(); 
         boolean isPrime = true;
 
         
@@ -14,7 +14,7 @@ public class PrimeNumber_Check {
 
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
-                    isPrime = false; // Found a divisor, not prime
+                    isPrime = false; 
                     break;
                 }
             }
