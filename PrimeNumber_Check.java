d99bc8ba8f9ca864bbadd8b47e01adc8e8a3b838
Srivallinayagam <srivallinayagamvs@gmail.com>
2025-05-07 13:56:13 +0530

    Prime_Number_or_Not

---
+++ PrimeNumber_Check.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNumber_Check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check Prime:");
+        int number = sc.nextInt(); // Example number
+        boolean isPrime = true;
+
+        
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false; // Found a divisor, not prime
+                    break;
+                }
+            }
+        }
+
+        
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
